2b170f1957f1b3cf6be0edf2f65c9fe853d69221
Andrew H <ahennesen@gmail.com>
2019-07-15 15:32:54 -0400

    fixed dbBook in bookController

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -6,32 +6,32 @@ module.exports = {
     db.Book
       .find(req.query)
       .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
+      .then(dbModel => res.json(dbBook))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
     db.Book
       .findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
+      .then(dbModel => res.json(dbBook))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
     db.Book
       .create(req.body)
-      .then(dbModel => res.json(dbModel))
+      .then(dbModel => res.json(dbBook))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
     db.Book
       .findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
+      .then(dbModel => res.json(dbBook))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
     db.Book
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
+      .then(dbModel => res.json(dbBook))
       .catch(err => res.status(422).json(err));
   }
 };
